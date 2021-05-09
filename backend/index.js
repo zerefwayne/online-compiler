7dad200b7d018f3343a0244131cc0a9efc9d8c03
Aayush Joglekar <aayushjog@gmail.com>
2021-05-09 16:36:52 +0530

    [language] Added Python Language

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 
 const { generateFile } = require("./generateFile");
 const { executeCpp } = require("./executeCpp");
+const { executePy } = require("./executePy");
 
 const app = express();
 
@@ -10,12 +11,10 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  return res.json({ hello: "world!" });
-});
-
 app.post("/run", async (req, res) => {
   const { language = "cpp", code } = req.body;
+
+  console.log(language, "Length:", code.length);
 
   if (code === undefined) {
     return res.status(400).json({ success: false, error: "Empty code body!" });
@@ -24,8 +23,12 @@ app.post("/run", async (req, res) => {
     // need to generate a c++ file with content from the request
     const filepath = await generateFile(language, code);
     // we need to run the file and send the response
-    const output = await executeCpp(filepath);
-
+    let output;
+    if (language === "cpp") {
+      output = await executeCpp(filepath);
+    } else if (language === "py") {
+      output = await executePy(filepath);
+    }
     return res.json({ filepath, output });
   } catch (err) {
     res.status(500).json({ err });
--- client/src/App.js
+++ client/src/App.js
@@ -5,23 +5,39 @@ import React, { useState } from "react";
 function App() {
   const [code, setCode] = useState("");
   const [output, setOutput] = useState("");
+  const [language, setLanguage] = useState("cpp");
 
   const handleSubmit = async () => {
     const payload = {
-      language: "cpp",
+      language,
       code,
     };
     try {
+      setOutput("");
       const { data } = await axios.post("http://localhost:5000/run", payload);
       setOutput(data.output);
-    } catch (err) {
-      console.log(err.response);
+    } catch ({ response }) {
+      const errMsg = response.data.err.stderr;
+      setOutput(errMsg);
     }
   };
 
   return (
     <div className="App">
       <h1>Online Code Compiler</h1>
+      <div>
+        <label>Language:</label>
+        <select
+          value={language}
+          onChange={(e) => {
+            setLanguage(e.target.value);
+          }}
+        >
+          <option value="cpp">C++</option>
+          <option value="py">Python</option>
+        </select>
+      </div>
+      <br />
       <textarea
         rows="20"
         cols="75"
